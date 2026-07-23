2698c7a6c75b238a1635347a30b1e71fdcec7e68
NeonFuzion <106327057+NeonFuzion@users.noreply.github.com>
2026-07-22 19:13:37 -0700

    Shadow Fix

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] float dampening;
+    [SerializeField] Transform target;
+
+    Vector2 velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = Vector2.SmoothDamp(transform.position, target.position, ref velocity, dampening);
+        transform.position = new (position.x, position.y, -10);
+    }
+}
